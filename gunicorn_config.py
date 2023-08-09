2bf5f9d82b8f1674ba77f6937bf0401dfc40adb1
Phuc Do <dhoangphuc237@gmail.com>
2023-08-09 10:30:18 +0700

    Debug

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,16 @@
+from decouple import Config
+
+# Initialize the Config class
+config = Config()
+
+# Load environment variables from .env file
+APP_ENV = config('APP_ENV', default='')
+DEBUG = config('DEBUG', default=False, cast=bool)
+AUTH0_API_DOMAIN = config('AUTH0_API_DOMAIN', default='')
+DWH = config('DWH', default='')
+APP_DOMAIN = config('APP_DOMAIN', default='')
+PORT = config('PORT', default='')
+
 workers = 4
 bind = 'localhost:8000'
 accesslog = './logs/access.log'
