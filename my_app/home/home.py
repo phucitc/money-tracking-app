9fc9b0614e4a4eec2b25799aefccb93bd81a2ff6
Phuc Do <dhoangphuc237@gmail.com>
2023-08-14 13:34:37 +0700

    Debug

--- my_app/home/home.py
+++ my_app/home/home.py
@@ -41,6 +41,7 @@ def download_qrcode(url_public_id):
     print("AAAAAAAAAAAAAAAAAAAAa")
     url = URL()
     item = url.get_by_public_id(url_public_id)
+    print('item.qrcode_path', item.qrcode_path)
     if item and is_empty(item.qrcode_path) is False:
         root_path = os.path.dirname(os.path.abspath(__file__))
         # go up multi level
